ce9961d2f2c7f5fb6be1e58041c887d890f29b9f
pbp2996 <pbp2996@yahoo.com>
2017-04-07 18:43:36 -0400

    keeping track of step with database & function

--- functions.php
+++ functions.php
@@ -4,15 +4,12 @@
 function getchatlog() {
 	include('database.php');
 
-	{
 	$statement = $db->prepare('SELECT * FROM chat');
     $statement->execute(); 
 	$chats = $statement->fetchall();
 	$statement->closeCursor();
 
 	return $chats;
-
-	}
 }
 
 //to add user input into chat tabel
@@ -54,22 +51,49 @@ function clearchat() {
 
 }
 
+//functino to get current step 
+function get_current_step(){
+	include('database.php');	
+	$statement = $db->prepare("SELECT * FROM pizzabot.steps
+								WHERE completed = 'N'
+								ORDER BY stepid ASC
+								LIMIT 1;");
+    $statement->execute(); 
+	$step = $statement->fetch();
+	$statement->closeCursor();
+
+	$currentstep = $step['stepid'];
+	return $currentstep;
+}
+
+function next_step($step_num){
+	include('database.php');
+
+	$query = "UPDATE steps SET completed= 'Y' WHERE stepid = :step_num;";
+	$statement = $db->prepare($query);
+	$statement->bindValue(':step_num', $step_num);
+	$statement->execute();
+	$statement->closeCursor();	
+}
+
 //to go through user string
 function search_through_and_respond($input) {
 	include('database.php');
-	global $currentstep;
 	
 	$response = "";
 	//step 1 validation - to add order 
 
-		if ($currentstep == "currentstep"){
-			if ((strpos($input,"order now") !== false) || (strpos($input,"place order") !== false) || (strpos($input,"order pizza") !== false)) {
-				$currentstep = "menu";
+		$currentstep = get_current_step(); 
+
+			if (($currentstep == 1) && ((strpos($input,"order now") !== false) || (strpos($input,"place order") !== false) || (strpos($input,"order pizza") !== false))) {
 				$response = "Lets do it, hit me with that order when you are ready...";
+				$step_num = $currentstep;
+				next_step($step_num);
+
 			} else {
 				$response = "sorry we already did that";
 			}
-		}
+
 
     $query = 'INSERT INTO chat
                  (input, `text`, img)
